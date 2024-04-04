51e5a87b30a19a18af68c49a7ee567f254f5b5cf
simon.montt <simon.montt@gmail.com>
2024-04-04 15:10:08 -0300

    data ingestion added

--- template.py
+++ template.py
@@ -9,12 +9,13 @@ project_name = "cnnClassifier"
 list_of_files =[
     ".githup/workflows/.gitkeep", #For CI/CD Deployment
     f"src/{project_name}/__init__.py", # Constructor file to create this folder as local package
-    f"src/{project_name}/components/_init_.py", # Folder for Data Ingestions/Data Validation/Model Trainer/
-    f"src/{project_name}/utils/_init_.py", # Utility code
-    f"src/{project_name}/config/_init_.py", # 
+    f"src/{project_name}/components/__init__.py", # Folder for Data Ingestions/Data Validation/Model Trainer/
+    f"src/{project_name}/utils/__init__.py", # Utility code
+    f"src/{project_name}/config/__init__.py", # 
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/_init_.py",
-    f"src/{project_name}/constants/_init_.py",
+    f"src/{project_name}/pipeline/__init__.py"
+    f"src/{project_name}/entity/__init__.py,"
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
